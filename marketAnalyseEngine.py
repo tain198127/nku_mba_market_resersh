79a752cd5ecef4846cbd3b08d1b2cbd8d3fafd70
DaneBrown <tain198127@163.com>
2018-12-19 18:03:00 +0800

    初步完成市场研究汇总工作

---
+++ marketAnalyseEngine.py
@@ -0,0 +1,266 @@
+import logging
+import os
+import random
+import sys
+
+import numpy
+import openpyxl
+import xlrd
+
+logging.basicConfig(level=logging.FATAL)
+family_name = ['赵', '钱', '孙', '李', '周', '吴', '郑', '王', '冯', '陈', '褚', '卫', '蒋', '沈', '韩', '杨', '朱', '秦', '尤', '许',
+               '何', '吕', '施', '张', '孔', '曹', '严', '华', '金', '魏', '陶', '姜', '戚', '谢', '邹', '喻', '柏', '水', '窦', '章',
+               '云', '苏', '潘', '葛', '奚', '范', '彭', '郎', '鲁', '韦', '昌', '马', '苗', '凤', '花', '方', '俞', '任', '袁', '柳',
+               '酆', '鲍', '史', '唐', '费', '廉', '岑', '薛', '雷', '贺', '倪', '汤', '滕', '殷', '罗', '毕', '郝', '邬', '安', '常',
+               '乐', '于', '时', '傅', '皮', '卞', '齐', '康', '伍', '余', '元', '卜', '顾', '孟', '平', '黄', '和', '穆', '萧', '尹',
+               '姚', '邵', '堪', '汪', '祁', '毛', '禹', '狄', '米', '贝', '明', '臧', '计', '伏', '成', '戴', '谈', '宋', '茅', '庞',
+               '熊', '纪', '舒', '屈', '项', '祝', '董', '梁']
+
+first_names = [
+    '子', '中', '生', '国', '年', '和',
+    '会', '家', '可', '天', '去', '能', '多', '然', '于', '心', '学', '都',
+    '看', '发', '当', '成', '只', '如', '事', '还', '用', '第', '样', '道', '想', '作', '种', '开', '美',
+    '总', '从', '己', '面', '前', '些', '同', '日', '又', '行', '意', '动',
+    '方', '期', '头', '经', '长', '儿', '回', '位', '分', '爱', '因', '名', '法', '间', '斯', '知',
+    '世', '者', '高', '已', '亲', '其', '进', '此', '常', '与', '活', '正', '感',
+    '见', '明', '问', '力', '理', '尔', '点', '文', '几', '定', '本', '公', '特', '外', '相', '西', '果', '走',
+    '将', '月', '十', '实', '向', '声', '车', '全', '信', '重', '三', '机', '工', '物', '气', '真',
+    '太', '新', '比', '才', '夫', '再', '书', '部', '水', '体', '却', '加', '电', '门',
+    '海', '听', '表', '德', '少', '克', '代', '员', '许', '稜', '先', '安',
+    '光', '白', '住', '望', '教', '命', '花', '结', '乐', '东', '神', '记', '处', '让',
+    '直', '平', '报', '友', '关', '放', '至', '入', '笑', '内', '英', '军',
+    '候', '民', '岁', '往', '何', '度', '山', '觉', '路', '带', '万', '男', '边', '风', '解', '叫', '任', '金', '快', '原',
+    '通', '师', '立', '象', '数', '四', '失', '满', '战', '远', '格', '士', '音', '轻', '目',
+    '始', '达', '深', '完', '今', '提', '求', '清', '王', '化', '空', '业', '思', '切', '非', '找', '罗',
+    '钱', '语', '元', '喜', '曾', '离', '飞', '科', '言', '干', '流', '欢', '约', '各', '即', '指', '合',
+    '题', '必', '该', '论', '交', '终', '林', '医', '晚', '制', '球', '决', '窢', '传', '画', '保', '读', '运', '及',
+    '则', '房', '早', '院', '量', '苦', '火', '布', '品', '近', '坐', '产', '答', '星', '精', '视', '五', '连', '司', '巴',
+    '奇', '管', '类', '未', '朋', '且', '婚', '台', '夜', '青', '北', '队', '久', '乎', '越', '观', '落', '尽', '形', '影',
+    '红', '爸', '百', '令', '周', '吧', '识', '步', '希', '亚', '术', '留', '市', '半', '热', '送', '兴', '造', '谈', '容',
+    '极', '随', '演', '收', '首', '根', '讲', '整', '式', '取', '照', '办', '强', '石', '古', '华', '諣', '拿', '计', '您',
+    '装', '似', '足', '双', '妻', '尼', '转', '诉', '米', '称', '丽', '客', '南', '领', '节', '衣', '站', '黑', '刻', '统',
+    '断', '福', '城', '故', '历', '惊', '脸', '选', '包', '紧', '争', '另', '建', '维', '绝', '树', '系', '伤', '示', '愿',
+    '持', '千', '史', '谁', '准', '联', '妇', '纪', '基', '买', '志', '静', '阿', '诗', '独', '复', '痛', '消', '社', '算',
+    '义', '竟', '确', '酒', '需', '单', '治', '卡', '幸', '兰', '念', '举', '仅', '钟', '怕', '共', '毛', '句', '息', '功',
+    '官', '待', '究', '跟', '穿', '室', '易', '游', '程', '号', '居', '考', '突', '皮', '哪', '费', '倒', '价', '图', '具',
+    '刚', '脑', '永', '歌', '响', '商', '礼', '细', '专', '黄', '块', '脚', '味', '灵', '改', '据', '般', '破', '引', '食',
+    '仍', '存', '众', '注', '笔', '甚', '某', '沉', '血', '备', '习', '校', '默', '务', '土', '微', '娘', '须', '试', '怀',
+    '料', '调', '广', '蜖', '苏', '显', '赛', '查', '密', '议', '底', '列', '富', '梦', '错', '座', '参', '八', '除', '跑',
+    '亮', '假', '印', '设', '线', '温', '虽', '掉', '京', '初', '养', '香', '停', '际', '致', '阳', '纸', '李', '纳', '验',
+    '助', '激', '够', '严', '证', '帝', '饭', '忘', '趣', '支', '春', '集', '丈', '木', '研', '班', '普', '导', '顿', '睡',
+    '展', '跳', '获', '艺', '六', '波', '察', '群', '皇', '段', '急', '庭', '创', '区', '奥', '器', '谢', '弟', '店', '否',
+    '害', '草', '排', '背', '止', '组', '州', '朝', '封', '睛', '板', '角', '况', '曲', '馆', '育', '忙', '质', '河', '续',
+    '哥', '呼', '若', '推', '境', '遇', '雨', '标', '姐', '充', '围', '案', '伦', '护', '冷', '警', '贝', '著', '雪', '索',
+    '剧', '船', '险', '烟', '依', '斗', '值', '帮', '汉', '佛', '肯', '闻', '唱', '沙', '局', '伯', '族', '低',
+    '玩', '资', '屋', '击', '速', '顾', '泪', '洲', '团', '圣', '旁', '堂', '兵', '七', '露', '园', '牛', '旅',
+    '劳', '型', '烈', '姑', '陈', '莫', '鱼', '异', '抱', '宝', '权', '鲁', '简', '态', '级', '票', '怪', '寻', '律',
+    '胜', '份', '汽', '右', '洋', '范', '床', '舞', '秘', '午', '登', '楼', '贵', '吸', '责', '例', '追', '较', '职', '属',
+    '渐', '左', '录', '丝', '牙', '党', '继', '托', '赶', '章', '智', '冲', '叶', '胡', '吉', '坚', '遗',
+    '修', '松', '临', '藏', '担', '戏', '善', '卫', '药', '悲', '敢', '伊', '村', '戴', '词', '森', '耳',
+    '祖', '云', '规', '散', '迷', '油', '适', '乡', '恩', '投', '弹', '铁', '博', '雷', '府',
+    '超', '勒', '杂', '醒', '洗', '采', '毫', '毕', '九', '冰', '既', '状', '乱', '景', '席', '珍', '童', '顶',
+    '派', '素', '脱', '农', '疑', '练', '野', '按', '征', '骨', '余', '承', '置', '臓', '彩', '灯', '巨',
+    '琴', '环', '技', '束', '增', '忍', '洛', '忆', '判', '欧', '层', '付',
+    '阵', '玛', '批', '岛', '项', '休', '懂', '武', '革', '良', '恶', '恋', '委', '拥', '娜', '妙', '探', '呀', '营',
+    '退', '摇', '弄', '桌', '熟', '诺', '宣', '银', '势', '奖', '宫', '忽', '套', '康', '供', '优', '课', '鸟',
+    '夏', '健', '模', '伴', '守', '挥', '鲜', '财', '孤', '枪', '杰',
+    '迹', '遍', '盖', '坦', '江', '顺', '秋', '萨', '授', '归', '浪', '凡', '预',
+    '雄', '升', '典', '莱', '含', '盛', '济', '蒙', '棋', '端', '释', '介', '烧',
+    '乾', '坤']
+
+
+class MarketAnalyseEngine:
+    @staticmethod
+    def __random_name():
+        """
+        获取一个随机名字
+        :return:
+        """
+        name = random.choice(family_name)
+        name = name + random.choice(first_names)
+        name_idx = random.randint(0, 1)
+        if name_idx > 0:
+            name = name + random.choice(first_names)
+        return name
+
+    @staticmethod
+    def __open_table(excel_path, sheet_idx):
+        """
+        :param excel_path: xlsx的地址
+        :param sheet_idx:  第几个sheet，从0开始
+        :return: sheet对象
+        """
+        data = xlrd.open_workbook(excel_path)
+        sheet = data.sheets()[sheet_idx]
+        return sheet
+
+    def __read_personal_info(self, excel_path):
+        """
+        读取个人信息数组
+        :param excel_path:
+        :return:个人信息数组
+        """
+        table = self.__open_table(excel_path, 0)
+        gander = table.cell_value(0, 1)
+        age = table.cell_value(1, 1)
+        location = table.cell_value(2, 1)
+        edu = table.cell_value(3, 1)
+        marital = table.cell_value(4, 1)
+        industry = table.cell_value(5, 1)
+        career = table.cell_value(6, 1)
+        lowest_consumption = table.cell_value(7, 1)
+        highest_comsumption = table.cell_value(7, 3)
+        device = table.cell_value(8, 1)
+        info = [gander, age, location, edu, marital, industry, career, lowest_consumption, highest_comsumption, device]
+        logging.debug("begin--------------" + sys._getframe().f_code.co_name)
+        logging.debug(info)
+        logging.debug("end--------------" + sys._getframe().f_code.co_name)
+        return info
+
+    def __read_detail_info(self, excel_path):
+        """
+        读取详细信息矩阵
+        :param excel_path:
+        :return:详细信息矩阵
+        """
+        detail = []
+        table = self.__open_table(excel_path, 1)
+        rows_count = 0
+        column_count = 48
+        real_column_count = 0
+        """
+        表明表头是两行
+        """
+        first_col = table.col_values(0)
+        first_row = table.row_values(2)
+        real_column_count = len(first_row)
+        for row in first_col:
+            if row != '-':
+                rows_count = rows_count + 1
+        logging.debug(rows_count)
+        # 48列
+        for row in range(2, rows_count):
+            row_values = table.row_values(row)
+            detail.append(row_values)
+
+        logging.debug(detail)
+        # 删掉第一行
+        detail = numpy.delete(detail, 1, axis=1)
+        logging.debug(detail)
+        # 删掉后面几列
+        detail = numpy.delete(detail, range(47, real_column_count - 1), axis=1)
+        logging.debug("begin--------------" + sys._getframe().f_code.co_name)
+        logging.debug(detail)
+        logging.debug("end--------------" + sys._getframe().f_code.co_name)
+        return detail
+
+    def __read_assemble_info(self, excel_path):
+        """
+        读取汇总信息
+        :param excel_path:  excel 文件路径
+        :return: 汇总信息
+        """
+        table = self.__open_table(excel_path, 2)
+        info = table.row_values(2)
+        logging.debug("begin--------------" + sys._getframe().f_code.co_name)
+        logging.debug(info)
+        logging.debug("end--------------" + sys._getframe().f_code.co_name)
+        return info
+
+    @staticmethod
+    def read_excels():
+        """
+        读取文件
+        :return:
+        """
+        excels = []
+        logging.debug("begin--------------" + sys._getframe().f_code.co_name)
+        logging.debug(os.getcwd())
+        logging.debug("end--------------" + sys._getframe().f_code.co_name)
+        path = os.getcwd()
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                logging.debug("filename:[{}]".format(file))
+                logging.debug("ext:[{}]".format(os.path.splitext(file)[1]))
+                if os.path.splitext(file)[1] == ".xlsx":
+                    excel_path = os.path.join(root, file)
+                    excels.append(excel_path)
+        return excels
+
+    @staticmethod
+    def __writ_into_excel(file_name, person_info_ary, detail_matrix_ary, asm_info_ary):
+        """
+        写入excel
+        :param file_name:
+        :param person_info:
+        :param detail_info:
+        :param asm_info:
+        :return:
+        """
+        excel_file = os.path.join(os.getcwd(), file_name)
+
+        logging.debug(excel_file)
+        logging.debug(person_info_ary)
+        logging.debug(detail_matrix_ary)
+        logging.debug(asm_info_ary)
+
+        wb = openpyxl.Workbook()
+        sheet1 = wb.create_sheet(title="personal info", index=0)
+        sheet1.append(["name", "gander", "age", "local", "edu", "marital", "industry", "career", "lowest_consumption",
+                       "highest_comsumption", "devie"])
+        for person_info in person_info_ary:
+            sheet1.append(person_info)
+        sheet2 = wb.create_sheet(title="detail info", index=1)
+        sheet2.append(
+            ['姓名','app名称','6:00~9:00', '9:00~12:00', '12:00~14:00', '14:00~19:00', '19:00~23:00', '23:00~6:00', '在家', '上班路上-公交',
+             '上班路上-私家车', '旅游', '办公室', '出差-短途', '出差-飞机', '出差-高铁等', '晴天', '阴天', '刮风', '下雨', '下雪', '雾霾', '台风', '工作日',
+             '	周末', '	春节', '	国庆节', '	劳动节', '	清明	', '端午', '	情人节', '	中秋节', '	元旦', '	圣诞节',
+             '	万圣节', '	体育', '	娱乐', '	旅游', '	房产', '	汽车	', '美食', '	理财	', '邮件网络', '	社群网络',
+             '	平均每周使用多少次', '	平均为此APP付款金额(元/周)	', '平均每周使用时长(分钟/周)'])
+        for detail_matrix in detail_matrix_ary:
+            for detail_info in detail_matrix:
+                sheet2.append(detail_info.tolist())
+        sheet3 = wb.create_sheet(title="assembly info", index=2)
+        sheet3.append(['姓名','6:00~9:00', '9:00~12:00', '12:00~14:00', '14:00~19:00', '19:00~23:00', '23:00~6:00', '在家', '上班路上-公交',
+             '上班路上-私家车', '旅游', '办公室', '出差-短途', '出差-飞机', '出差-高铁等', '晴天', '阴天', '刮风', '下雨', '下雪', '雾霾', '台风', '工作日',
+             '	周末', '	春节', '	国庆节', '	劳动节', '	清明	', '端午', '	情人节', '	中秋节', '	元旦', '	圣诞节',
+             '	万圣节', '	体育', '	娱乐', '	旅游', '	房产', '	汽车	', '美食', '	理财	'])
+        for asm_info in asm_info_ary:
+            sheet3.append(asm_info)
+        wb.save(excel_file)
+
+    def merge_into_excel(self, file_name):
+        """
+        主函数
+        :return:
+        """
+        excels = self.read_excels()
+        personal_merge_info = []
+        detail_merge_info = []
+        assemb_merge_info = []
+        for f in excels:
+            random_name = self.__random_name()
+            personal_info = self.__read_personal_info(f)
+            detail_info = self.__read_detail_info(f)
+            assemb_info = self.__read_assemble_info(f)
+            # 写入随机姓名
+            personal_info.insert(0, random_name)
+            detail_info = numpy.insert(detail_info, 0, values=random_name, axis=1)
+            assemb_info.insert(0, random_name)
+            # 打印日志
+            logging.debug(personal_info)
+            logging.debug(assemb_info)
+            logging.debug(detail_info)
+            personal_merge_info.append(personal_info)
+            detail_merge_info.append(detail_info)
+            assemb_merge_info.append(assemb_info)
+            # 写入
+        self.__writ_into_excel(file_name, personal_merge_info, detail_merge_info, assemb_merge_info)
+
+
+engin = MarketAnalyseEngine()
+docs = engin.merge_into_excel("asm.xlsx")
+# for file in docs:
+#
+#     engin.read_detail_info(file)
+#     engin.read_assemble_info(file)
